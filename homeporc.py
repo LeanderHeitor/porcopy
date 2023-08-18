fe0630b385f2a99f260e82465f9b0ff69a4ce285
LeanderHeitor <heitor.leander@hotmail.com>
2023-08-18 00:19:14 -0300

    segundaversao

--- homeporc.py
+++ homeporc.py
@@ -1,200 +1,175 @@
-from json import dumps, loads
 import os
-#mostrar todos os arquivos .txt de uma pasta
-def ListaDeArquivos():
+import json
+import tkinter as tk
+from tkinter import messagebox, simpledialog, scrolledtext
+
+def lista_de_arquivos():
     arquivos = os.listdir()
-    arquivos_txt = (arquivo for arquivo in arquivos if arquivo.endswith('.txt'))
-    ArquivoSemExtensao = (os.path.splitext(arquivo)[0] for arquivo in arquivos_txt)
-    return ArquivoSemExtensao
+    arquivos_txt = [arquivo for arquivo in arquivos if arquivo.endswith('.txt')]
+    arquivo_sem_extensao = [os.path.splitext(arquivo)[0] for arquivo in arquivos_txt]
+    return arquivo_sem_extensao
 
-def AdicionarPorcos():
-    idade = int(input("Digite a idade do seu animal: "))
-    sexo = input("Digite o sexo do seu animal: ")
-    raca = input("Digite a raça do seu animal: ")
-    pes = int(input("Digite o peso do seu animal(Kg): "))
-    racaoDia = int(input("Digite a quantidade de ração que o seu animal come por dia: "))
-    vacina = list()
-    op = input("O seu animal já tomou vacina?\n1. Sim\n2. Não\nEscolha uma opção: ")
-    if op == '1': #caso o animal já tenha tomado vacina, o usuário informa o nome da vacina e a quantidade de vacinas que o animal tomou
-        ask = int(input("Quantas vacinas o seu animal tomou?\n"))
-        for i in range(ask):
-            vacina.append(input("Digite o nome da vacina: "))
-        por[nom] = {'Idade': idade, 'Sexo': sexo, 'Raça': raca, 'Peso': pes, 'Ração por dia': racaoDia, 'Vacinas': vacina} #adiciona os dados do porco no dicionário
-        return por
-
-    elif op == '2': #caso o animal não tenha tomado vacina, o valor da chave Vacinas é vazio
-        por[nom] = {'Idade': idade, 'Sexo': sexo, 'Raça': raca, 'Peso': pes, 'Ração por dia': racaoDia, 'Vacinas': []} #adiciona os dados do porco no dicionário
-        return por
-    else:
-        print("Opção inválida")
-
-#menu principal
-while True:
-    ArquivoSemExtensao = list(ListaDeArquivos())
-    ordem = len(ArquivoSemExtensao)
-    for i,nome in enumerate(ArquivoSemExtensao, start=1):
-        print(f'{i}. {nome}')
-    print(f'{ordem+1}. Adicionar baia')
-    print(f'0. Sair')
-    ask = input("Escolha uma das opções: ")
-
-    #lista de numeros de 0 até o numero de arquivos + 1
-    lista = list(range(1,ordem+1))
-
-    #dicionário com os numeros da lista e os nomes dos arquivos
-    dicionario = dict(zip(lista, ArquivoSemExtensao))
-
-    #verifica se a opção escolhida está na lista
-    if int(ask) in lista:
-        baiaAberta = dicionario[int(ask)]
-        arquivo = str(baiaAberta)
+def adicionar_porcos(nome_arquivo):
+    idade = int(simpledialog.askstring("Idade", "Digite a idade do seu animal: "))
+    sexo = simpledialog.askstring("Sexo", "Digite o sexo do seu animal: ")
+    raca = simpledialog.askstring("Raça", "Digite a raça do seu animal: ")
+    peso = int(simpledialog.askstring("Peso", "Digite o peso do seu animal(Kg): "))
+    racao_dia = int(simpledialog.askstring("Ração por dia", "Digite a quantidade de ração que o seu animal come por dia: "))
     
-        f = open(f'{arquivo}.txt', 'r')
-        if f.read() == '': #verifica se o arquivo está vazio
-            por = dict()
-            while True:
-                print(f'1. Adicionar porco')
-                print(f'0. Sair')
-                ask = input("Escolha uma das opções: ")
-                if ask == '1':
-                    nom = input("Digite o nome do seu animal: ")
-                    por = AdicionarPorcos()
-                    f = open(f'{arquivo}.txt', 'w')
-                    f.write(dumps(por))
-                    f.close()
-                    break
-                elif ask == '0':
-                    break
-                else:
-                    print("Opção inválida")
-        else:
-            f = open(f'{arquivo}.txt', 'r')
-            por_disserializado = loads(f.read())
-            f.close()   
-            #menu secundário para adicionar, verificar ou remover porcos
-            while True:
-                f = open(f'{arquivo}.txt', 'r')
-                por_disserializado = loads(f.read())
-                f.close()
-                x = len(por_disserializado)+1
-                for i,porco in enumerate(por_disserializado, start=1): #mostra os porcos da baia
-                    print(f'{i}. {porco}')
-                print(f'{len(por_disserializado)+1}. Adicionar porco')
-                print(f'{len(por_disserializado)+2}. Remover baia')
-                print(f'0. Sair')
-
-                ask = input("Escolha uma das opções: ")
-
-                #lista de numeros de 0 até o numero de porcos + 1
-                lista = list(range(1,len(por_disserializado)+1))
-
-                #dicionário com os numeros da lista e os nomes dos porcos
-                dicionarioPorcos = dict(zip(lista, por_disserializado))
-
-                #verifica se a opção escolhida está na lista
-                if int(ask) in lista:
-                    porcoAberto = dicionarioPorcos[int(ask)]
-                    print(f'Identificação: {porco}')
-                    print(f"Idade: {por_disserializado[porco]['Idade']}")
-                    print(f"Sexo: {por_disserializado[porco]['Sexo']}")
-                    print(f"Raça: {por_disserializado[porco]['Raça']}")
-                    print(f"Peso: {por_disserializado[porco]['Peso']}")
-                    print(f"Ração por dia: {por_disserializado[porco]['Ração por dia']}")
-                    lista = ' ,'.join(por_disserializado[porco]['Vacinas'])
-                    print(f"Vacinas: {lista}")
-
-                    print('1. Atualizar dados')
-                    print('2. Remover porco')
-                    print('0. Sair')
-                    ask = input("Escolha uma das opções: ")
-                    if ask == '1':
-                        f = open(f'{arquivo}.txt', 'r')
-                        por_disserializado = loads(f.read())
-                        ask = input('''Qual dado deseja atualizar?
-                                                1. Idade
-                                                2. Peso
-                                                3. Ração por dia
-                                                4. Vacinas
-                                                Escolha uma opção: ''')
-                        if ask == '1':
-                                        por_disserializado[porcoAberto]['Idade'] = int(input("Digite a nova idade: "))
-                                        f = open(f'{arquivo}.txt', 'w')
-                                        f.write(dumps(por_disserializado))
-                                        f.close()
-                        elif ask == '2': 
-                                        por_disserializado[porco]['Peso'] = input("Digite o novo peso: ")
-                                        f = open(f'{arquivo}.txt', 'w')
-                                        f.write(dumps(por_disserializado))
-                                        f.close()
-                        elif ask == '3':
-                                        por_disserializado[porco]['Ração por dia'] = input("Digite a nova quantidade de ração por dia: ")
-                                        f = open(f'{arquivo}.txt', 'w')
-                                        f.write(dumps(por_disserializado))
-                                        f.close()
-                        elif ask == '4':
-                                        ask = input("Qual vacina deseja adicionar?\n")
-                                        por_disserializado[porcoAberto]['Vacinas'].append(ask) #adiciona a vacina na lista de vacinas
-                                        f = open(f'{arquivo}.txt', 'w')
-                                        f.write(dumps(por_disserializado)) #serializa os dados do arquivo e escreve no arquivo
-                                        f.close()
-                                    
-                        else:
-                                        print("Opção inválida!")
-                    elif ask == '2':
-                        f = open(f'{arquivo}.txt', 'r')
-                        por_disserializado = loads(f.read())
-                        f.close()
-                        del por_disserializado[porcoAberto]
-                        f = open(f'{arquivo}.txt', 'w')
-                        f.write(dumps(por_disserializado))
-                        print('Porco removido com sucesso!')
-                
-                elif ask == str(len(por_disserializado)+1): #adiciona um porco
-                    por = {}
-                    nom = input("Digite uma identificação para o animal: ")
-                    if nom in por_disserializado:
-                        print('Identificação já cadastrada!')
-                        break
-                    else:
-                        por = AdicionarPorcos()
-
-                        f = open(f'{arquivo}.txt', 'r')
-                        if f.read() == '': #verifica se o arquivo está vazio
-                            f.close()
-                            f = open(f'{arquivo}.txt', 'w')
-                            f.write(dumps(por))
-                        else:
-                            f.close
-                            f = open(f'{arquivo}.txt', 'r')
-                            por_disserializado = loads(f.read()) #caso não esteja vazio, carrega os dados do arquivo
-                            f.close()
-                            por_disserializado.update(por) #adiciona os dados do porco no dicionário
-                            f = open(f'{arquivo}.txt', 'w')
-                            por_disserializado = dumps(por_disserializado) #serializa os dados do arquivo
-                            f.write(por_disserializado) #escreve os dados no arquivo
-                        f.close()
-                elif ask == str(len(por_disserializado)+2):
-                    f = open(f'{arquivo}.txt', 'r')
-                    f.close()
-                    os.remove(f'{arquivo}.txt') #remove o arquivo
-                    print("Baia removida com sucesso!")
-                    break
-                elif ask == '0':
-                    break
-                else:
-                    print("Opção inválida!")
-    #adiciona baia
-    elif ask == str(ordem+1):
-        print(f'''1. Adicionar baia
-              2. Voltar ao Menu Principal''')
-        ask = input("Escolha uma das opções: ")
-        if ask == '1':
-            nome = input('Adicione o nome da baia:')
+    vacina = []
+    op = tk.messagebox.askyesno("Vacina", "O seu animal já tomou vacina?")
+    
+    if op:
+        ask = int(simpledialog.askstring("Vacinas", "Quantas vacinas o seu animal tomou?"))
+        for _ in range(ask):
+            vacina.append(simpledialog.askstring("Vacinas", "Digite o nome da vacina: "))
+    
+    por = {'Idade': idade, 'Sexo': sexo, 'Raça': raca, 'Peso': peso, 'Ração por dia': racao_dia, 'Vacinas': vacina}
+    
+    try:
+        with open(f'{nome_arquivo}.txt', 'r+') as f:
             try:
-                with open(f'{nome}.txt', 'x') as file: #verifica se arquivo como nome da baia já existe
-                    print("Baia cadastrada com sucesso!")
-            except FileExistsError: 
-                print("Baia já cadastrada!") #caso exista, retorna mensagem de erro
+                dados = json.load(f)
+            except json.JSONDecodeError:
+                dados = {}  # Se o JSON estiver vazio ou inválido, inicializa com um dicionário vazio
+    except FileNotFoundError:
+        dados = {}
+    
+    identificacao = simpledialog.askstring("Identificação", "Digite uma identificação para o animal: ")
+    dados[identificacao] = por
+    
+    with open(f'{nome_arquivo}.txt', 'w') as f:
+        json.dump(dados, f)
 
-    elif ask == '0':
-        break
+def remover_baia(nome_baia):
+    try:
+        os.remove(f'{nome_baia}.txt')
+        arquivo_sem_extensao.remove(nome_baia)
+        baia_listbox.delete(tk.ACTIVE)
+        messagebox.showinfo("Sucesso", f"Baia '{nome_baia}' removida com sucesso!")
+    except FileNotFoundError:
+        messagebox.showerror("Erro", f"Baia '{nome_baia}' não encontrada!")
+
+def listar_porcos(nome_arquivo):
+    try:
+        with open(f'{nome_arquivo}.txt', 'r') as f:
+            conteudo = f.read()
+            if conteudo.strip() == "":
+                return []  # Retorna uma lista vazia se o arquivo estiver vazio
+            dados = json.loads(conteudo)
+            porcos = []
+            for identificacao, porco in dados.items():
+                porcos.append(f"Identificação: {identificacao}, Idade: {porco['Idade']} anos, Sexo: {porco['Sexo']}")
+            return porcos
+    except FileNotFoundError:
+        return []
+
+def remover_porco(nome_arquivo, identificacao):
+    try:
+        with open(f'{nome_arquivo}.txt', 'r') as f:
+            dados = json.load(f)
+        if identificacao in dados:
+            del dados[identificacao]
+            with open(f'{nome_arquivo}.txt', 'w') as f:
+                json.dump(dados, f)
+            messagebox.showinfo("Sucesso", f"Porco '{identificacao}' removido com sucesso!")
+            listar_porcos_baia(nome_arquivo)  # Atualiza a lista de porcos na baia após a remoção
+        else:
+            messagebox.showerror("Erro", f"Porco '{identificacao}' não encontrado!")
+    except FileNotFoundError:
+        messagebox.showerror("Erro", "Arquivo da baia não encontrado!")
+    except json.JSONDecodeError:
+        messagebox.showerror("Erro", "Erro ao carregar dados da baia!")
+
+def listar_porcos_baia(nome_arquivo):
+    try:
+        with open(f'{nome_arquivo}.txt', 'r') as f:
+            conteudo = f.read()
+            if conteudo.strip() == "":
+                return []  # Retorna uma lista vazia se o arquivo estiver vazio
+            dados = json.loads(conteudo)
+            porcos = []
+            for identificacao, porco in dados.items():
+                porcos.append(f"Identificação: {identificacao}, Idade: {porco['Idade']} anos, Sexo: {porco['Sexo']}")
+            return porcos
+    except FileNotFoundError:
+        return []
+
+def main():
+    root = tk.Tk()
+    root.title("Gerenciamento de Porcos")
+    root.geometry("1280x720")  # Definindo o tamanho da janela
+
+    arquivo_sem_extensao = lista_de_arquivos()
+
+    def listar_baias():
+        baia_listbox.delete(0, tk.END)
+        for i, nome in enumerate(arquivo_sem_extensao, start=1):
+            baia_listbox.insert(tk.END, f'Baia {i}: {nome}')
+
+    def adicionar_baia():
+        nome_baia = simpledialog.askstring("Adicionar Baia", "Adicione o nome da baia:")
+        if nome_baia:
+            try:
+                with open(f'{nome_baia}.txt', 'x'):
+                    messagebox.showinfo("Sucesso", "Baia cadastrada com sucesso!")
+                    arquivo_sem_extensao.append(nome_baia)
+                    listar_baias()
+            except FileExistsError:
+                messagebox.showerror("Erro", "Baia já cadastrada!")
+
+    def baia_selecionada_event(event):
+        if baia_listbox.curselection():
+            selected_index = baia_listbox.curselection()[0]
+            nome_baia = arquivo_sem_extensao[selected_index]
+            baia_label.config(text=f"Baia selecionada: {nome_baia}")
+            baia_frame.pack()
+            adicionar_porco_button.config(command=lambda: adicionar_porcos(nome_baia))
+            listar_porcos_button.config(command=lambda: listar_porcos_baia(nome_baia))
+            remover_porco_button.config(command=lambda: remover_porco(nome_baia, identificacao_entry.get()))
+            baia_listbox2.delete(0, tk.END)  # Limpa a lista de porcos quando uma baia é selecionada
+            porcos_text.config(state=tk.DISABLED)  # Desabilita a caixa de texto dos porcos
+
+            # Lista os porcos da baia selecionada
+            porcos = listar_porcos_baia(nome_baia)
+            porcos_text.config(state=tk.NORMAL)
+            porcos_text.delete(1.0, tk.END)
+            if porcos:
+                porcos_text.insert(tk.END, "\n".join(porcos))
+            else:
+                porcos_text.insert(tk.END, "Não há porcos nesta baia.")
+            porcos_text.config(state=tk.DISABLED)
+
+    baia_listbox = tk.Listbox(root, font=("Helvetica", 14))
+    baia_listbox2 = tk.Listbox(root, font=("Helvetica", 14))
+    listar_baias()
+
+    baia_frame = tk.Frame(root)
+    baia_label = tk.Label(baia_frame, text="Baia selecionada:", font=("Helvetica", 16))
+    adicionar_porco_button = tk.Button(baia_frame, text="Adicionar Porco", font=("Helvetica", 14))
+    listar_porcos_button = tk.Button(baia_frame, text="Listar Porcos", font=("Helvetica", 14))
+    remover_porco_button = tk.Button(baia_frame, text="Remover Porco", font=("Helvetica", 14))
+    identificacao_entry = tk.Entry(baia_frame, font=("Helvetica", 12))  # Campo de entrada para a identificação do porco a ser removido
+    porcos_text = scrolledtext.ScrolledText(baia_frame, state=tk.DISABLED, height=10, width=60, font=("Helvetica", 12))  # Caixa de texto para listar porcos
+
+    baia_label.pack()
+    adicionar_porco_button.pack()
+    identificacao_entry.pack()  # Adicionar o campo de entrada de identificação
+    remover_porco_button.pack()
+    listar_porcos_button.pack()
+    porcos_text.pack()
+
+    adicionar_baia_button = tk.Button(root, text="Adicionar Baia", command=adicionar_baia, font=("Helvetica", 14))
+    sair_button = tk.Button(root, text="Sair", command=root.destroy, font=("Helvetica", 14))
+
+    baia_listbox.pack()
+    baia_listbox2.pack()
+    adicionar_baia_button.pack()
+    sair_button.pack()
+
+    baia_listbox.bind("<<ListboxSelect>>", baia_selecionada_event)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
